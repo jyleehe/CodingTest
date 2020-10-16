67c7fac8aa4e448c99efdabc09944ad6f9efafad
jyleehe <ljyn333@gmail.com>
2020-10-16 20:31:41 +0900

    solved. encryption. added comments.

--- Inmybrain_encryption.py
+++ Inmybrain_encryption.py
@@ -1,20 +1,21 @@
 
-
 def solution(enc, kstring):
     res = ''
-    print(enc, kstring)
     target = enc
 
     for k in kstring:
-        # 나머지에서
+        # 나머지 문자열에서 키값 찾기
         for i, s in enumerate(target):
             if k != s: # for original string
                 res += s
             else: # for key
-                target = target[i+1:] # skip this char.
-                break
-    res += target
+                target = target[i+1:] # 키값은 복호화 skip
+                break # 다음 키값으로 이동
+
+    res += target # key 모두 찾고 마지막 남은 문자열 반영
     return res
+
+
 
 if __name__ == "__main__":
     inputs = []
@@ -33,3 +34,4 @@ if __name__ == "__main__":
         print("correct?: {}".format(ansarr == an))
         print("predict: {}".format(ansarr))
         print("answer: {}".format(an))
+        print("")
